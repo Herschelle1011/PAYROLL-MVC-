86ab5dcef03e9bfec155da1aeae327d1170c656f
DESKTOP-7DGVFCB\DELL <herschellee101@gmail.com>
2025-12-09 15:13:45 +0800

    test1

---
+++ Payroll System/Views/Account/Registercshtml.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Payroll_System.Views.Account
+{
+    public class RegistercshtmlModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
